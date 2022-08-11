025d262c72c9b148484d449acc899bab35d76d62
신창호 <vkdghckdh@gmail.com>
2022-08-11 20:42:42 +0900

    📰 Add: 초기 프로젝트 등록

---
+++ server/src/main/java/com/developer/santa/SantaApplication.java
@@ -0,0 +1,13 @@
+package com.developer.santa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/developer/santa/SantaApplicationTests.java
@@ -0,0 +1,20 @@
+package com.developer.santa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		//given
+
+		// when
+
+		// then
+
+	}
+
+}
