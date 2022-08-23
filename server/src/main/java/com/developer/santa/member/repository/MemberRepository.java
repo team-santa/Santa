b50eb4e2454edb673a096dc24ced7b4489f18bdc
minoolian <mw.lee@kakao.com>
2022-08-24 03:03:17 +0900

    :memo: Refactor: Entity 조회시 Optional 반환

--- server/src/main/java/com/developer/santa/member/repository/MemberRepository.java
+++ server/src/main/java/com/developer/santa/member/repository/MemberRepository.java
@@ -3,8 +3,9 @@ package com.developer.santa.member.repository;
 import com.developer.santa.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByUsername(String username);
+import java.util.Optional;
 
-    Member findByUserId(String userId);
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+    Optional<Member> findByUserId(String userId);
 }
