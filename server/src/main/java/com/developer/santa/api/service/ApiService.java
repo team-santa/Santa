d0d5ebe05a1d062923d592d5a57df9d25230c431
5witchkr <hob4419@gmail.com>
2022-08-26 09:56:43 +0900

    :recycle:Refactor: apiCrawling

--- server/src/main/java/com/developer/santa/api/service/ApiService.java
+++ server/src/main/java/com/developer/santa/api/service/ApiService.java
@@ -2,33 +2,25 @@ package com.developer.santa.api.service;
 
 import com.developer.santa.api.domain.batchdata.BatchData;
 import com.developer.santa.api.domain.batchdata.BatchRepository;
-import com.developer.santa.api.domain.course.Course;
 import com.developer.santa.api.domain.course.CourseRepository;
-import com.developer.santa.api.domain.local.Local;
 import com.developer.santa.api.domain.local.LocalRepository;
-import com.developer.santa.api.domain.mountain.Mountain;
 import com.developer.santa.api.domain.mountain.MountainRepository;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationEvent;
+import org.springframework.beans.factory.annotation.Value;;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.event.EventListener;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
-
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
+
 
 
 @Service
@@ -46,36 +38,33 @@ public class ApiService {
 
     private final MountainRepository mountainRepository;
     private final LocalRepository localRepository;
-    private final CourseRepository courseRepository;
     private final BatchRepository batchRepository;
     private final ApplicationEventPublisher eventPublisher;
-
 
     public String connectApi(String geomFilter, String crs, int page, int size, String localName) {
         if (size > 20) return String.valueOf(webClientApi(geomFilter, crs, page, size));
 
-        JSONObject connectApiData = webClientApi(geomFilter, crs, page, size);
-        isExistsUrl(geomFilter, crs, page, size, localName, connectApiData);
-        return String.valueOf(connectApiData);
+//        JSONObject connectApiData = webClientApi(geomFilter, crs, page, size);
+//        isExistsUrl(geomFilter, crs, page, size, localName);
+        return String.valueOf(webClientApi(geomFilter, crs, page, size));
     }
 
 
     public void dataCrawling(String geomFilter, String crs, int page, int size, String localName){
-        JSONObject connectApiData = webClientApi(geomFilter, crs, page, size);
-        isExistsUrl(geomFilter, crs, page, size, localName, connectApiData);
+        isExistsUrl(geomFilter, crs, page, size, localName);
     }
 
 
-    private void isExistsUrl(String geomFilter, String crs, int page, int size, String localName, JSONObject connectApiData) {
+    private void isExistsUrl(String geomFilter, String crs, int page, int size, String localName) {
         String reqUrl = "/req/data?service=data&request=GetFeature&data=LT_L_FRSTCLIMB"+"&geomFilter="+ geomFilter +"&crs="+ crs +"&size="+ size +"&page="+ page;
 
         if(batchRepository.existsByReqUrl(reqUrl)) {
             return;
         }
         batchRepository.save(new BatchData(reqUrl));
-        eventPublisher.publishEvent(new DataSaveRamda(eventPublisher,
+        eventPublisher.publishEvent(new DataCrawlingEvent(eventPublisher,
                 localName,
-                connectApiData.getJSONArray("features"),
+                webClientApi(geomFilter, crs, page, size).getJSONArray("features"),
                 mountainRepository,
                 localRepository));
     }
@@ -112,60 +101,5 @@ public class ApiService {
                 .getJSONObject("response")
                 .getJSONObject("result")
                 .getJSONObject("featureCollection");
-    }
-
-
-    @Getter
-    static class DataSaveRamda extends ApplicationEvent {
-        private final String localName;
-        private final JSONArray parsingval;
-        private final MountainRepository mountainRepository;
-        private final LocalRepository localRepository;
-        public DataSaveRamda(
-                Object source,
-                String localName,
-                JSONArray parsingval,
-                MountainRepository mountainRepository,
-                LocalRepository localRepository) {
-            super(source);
-            this.localName=localName;
-            this.parsingval=parsingval;
-            this.mountainRepository=mountainRepository;
-            this.localRepository=localRepository;
-        }
-    }
-
-
-    @EventListener()
-    public void saveLocalMountainCourse(DataSaveRamda dataSaveRamda){
-        IntStream.range(0, dataSaveRamda.getParsingval().length())
-                .mapToObj(index -> (JSONObject) dataSaveRamda.getParsingval().get(index))
-                .forEach(obj -> {
-                    String location = String.valueOf(obj
-                            .getJSONObject("geometry").getJSONArray("coordinates")
-                            .getJSONArray(0).getJSONArray(0));
-                    String mountain = obj.getJSONObject("properties").getString("mntn_nm");
-                    String level = obj.getJSONObject("properties").getString("cat_nam");
-                    String distance = obj.getJSONObject("properties").getString("sec_len");
-                    if(!localRepository.existsByLocalName(dataSaveRamda.getLocalName())){
-                        localRepository.save(new Local(dataSaveRamda.getLocalName()));
-                    }
-                    if(!mountainRepository.existsByMountainName(mountain)) {
-                        mountainRepository.save(new Mountain(mountain, localRepository.findByLocalName(dataSaveRamda.getLocalName())));
-                    }
-                    if(!courseRepository.existsByCourseLocation(location)) {
-                        for (char i=65; i<117; i++){
-                            if (i == 91) i+=6;
-                            if (!courseRepository.existsByCourseName(mountain+" 등산로 "+i)){
-                                courseRepository.save(new Course(mountain+" 등산로 "+i,
-                                        location,
-                                        level,
-                                        distance,
-                                        mountainRepository.findByMountainName(mountain)));
-                                break;
-                            }
-                        }
-                    }
-                });
     }
 }
---
+++ server/src/main/java/com/developer/santa/api/service/DataCrawlingEvent.java
@@ -0,0 +1,27 @@
+package com.developer.santa.api.service;
+
+import com.developer.santa.api.domain.local.LocalRepository;
+import com.developer.santa.api.domain.mountain.MountainRepository;
+import lombok.Getter;
+import org.json.JSONArray;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+class DataCrawlingEvent extends ApplicationEvent {
+    private final String localName;
+    private final JSONArray parsingval;
+    private final MountainRepository mountainRepository;
+    private final LocalRepository localRepository;
+    public DataCrawlingEvent(
+            Object source,
+            String localName,
+            JSONArray parsingval,
+            MountainRepository mountainRepository,
+            LocalRepository localRepository) {
+        super(source);
+        this.localName=localName;
+        this.parsingval=parsingval;
+        this.mountainRepository=mountainRepository;
+        this.localRepository=localRepository;
+    }
+}
