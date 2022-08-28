87aa4668ff94cb67771fa8cf0fc798b25de7282c
minoolian <mw.lee@kakao.com>
2022-08-29 01:41:27 +0900

    :white_check_mark: Test: OAuth2 로그인 테스트 관련 Provider snippet 추가

--- server/src/main/java/com/developer/santa/member/oauth/entity/ProviderType.java
+++ server/src/main/java/com/developer/santa/member/oauth/entity/ProviderType.java
@@ -4,8 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum ProviderType {
-    GOOGLE,
-    KAKAO,
-    GITHUB,
-    NAVER
+    GOOGLE("google", "구글 로그인"),
+    KAKAO("kakao", "카카오 로그인"),
+    GITHUB("github", "깃허브 로그인"),
+    NAVER("naver", "네이버 로그인");
+
+    String providerId;
+    String decription;
+
+    ProviderType(String providerId, String decription) {
+        this.providerId = providerId;
+        this.decription = decription;
+    }
 }
