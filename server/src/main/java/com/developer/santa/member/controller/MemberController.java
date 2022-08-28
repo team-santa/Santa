6b1f518986a12b80b883cdcd74d1a898c87b8560
minoolian <mw.lee@kakao.com>
2022-08-29 01:39:01 +0900

    :recycle: Refactor: Member Controller 요청 API 변경

--- server/src/main/java/com/developer/santa/member/controller/MemberController.java
+++ server/src/main/java/com/developer/santa/member/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
