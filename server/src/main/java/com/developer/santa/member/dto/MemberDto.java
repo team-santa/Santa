3423219847f4a0af68f0c36e906b0459cb3bc3fc
minoolian <mw.lee@kakao.com>
2022-08-29 01:39:57 +0900

    :sparkles: Feat: Member Validation 추가

--- server/src/main/java/com/developer/santa/member/dto/MemberDto.java
+++ server/src/main/java/com/developer/santa/member/dto/MemberDto.java
@@ -3,13 +3,21 @@ package com.developer.santa.member.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public class MemberDto {
 
     @Getter
     @AllArgsConstructor
     public static class Response {
+
+        @NotBlank
         private String username;
+
+        @Email
         private String email;
+
         private String profileImageUrl;
     }
 }
