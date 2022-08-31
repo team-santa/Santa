c0c28e0b9aa869ead5946838a382400c558e561e
minoolian <mw.lee@kakao.com>
2022-09-01 01:58:42 +0900

    :sparkles: Feat: Username 중복확인 로직 추가

--- server/src/main/java/com/developer/santa/member/repository/MemberRepository.java
+++ server/src/main/java/com/developer/santa/member/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUsername(String username);
+    Boolean existsByUsername(String username);
     Optional<Member> findByMemberId(String memberId);
 }
