f09fac7814408ecbe98a066b219b62a493731782
신창호 <vkdghckdh@gmail.com>
2022-09-01 14:52:57 +0900

    :white_check_mark: Test: 더미데이터 (주석)추가

--- server/src/main/resources/h2/data.sql
+++ server/src/main/resources/h2/data.sql
@@ -1,3 +1,10 @@
+
+--INSERT INTO MEMBER (member_id, username, password, email, provider_type, role_type) VALUES
+--("1","test", "NO_PASS","NO_EMAIL", "KAKAO	" ,"USER"),
+--("2","test2", "NO_PASS2","NO_EMAIL2", "KAKAO" ,"USER");
+
+
+
 INSERT INTO REVIEW_BOARD (local_name, mountain_name, course_name, title, body, photo) VALUES
  ('서울시', '관악산', '학바위능선', '리뷰제목1', '임의의 내용', '임의의 사진'),
  ('서울시', '관악산', '관악산봉우리', '리뷰제목2', '임의의 내용2', '임의의 사진2'),
