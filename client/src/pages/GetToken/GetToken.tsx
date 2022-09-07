a6ec7451296b291d40b8567a2251ac55518005c6
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-07 12:10:40 +0900

    ✨Feat : 유저 정보 가져오는 로직 추가

--- client/src/pages/GetToken/GetToken.tsx
+++ client/src/pages/GetToken/GetToken.tsx
@@ -3,6 +3,14 @@ import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { setCookie } from "src/utils/cookie";
 import jwt_decode from "jwt-decode";
+import axios from "axios";
+import { User } from "src/types";
+
+interface Decode {
+  exp: number;
+  role: string;
+  sub: string;
+}
 
 const GetToken = () => {
   const [searchParams] = useSearchParams();
@@ -10,8 +18,16 @@ const GetToken = () => {
     const token = searchParams.get("token");
     if (token) {
       setCookie("token", token, { path: "/" });
-      const decoded = jwt_decode(token);
-      localStorage.setItem("user", JSON.stringify(decoded));
+      const decoded: Decode = jwt_decode(token);
+      axios.get(`http://localhost:8080/members/${decoded.sub}`).then((res) => {
+        const user: User = res.data;
+        const memberId = decoded.sub;
+        const mergeUser = {
+          ...user,
+          memberId,
+        };
+        localStorage.setItem("user", JSON.stringify(mergeUser));
+      });
     }
 
     if (searchParams.get("signup") === "true") {
