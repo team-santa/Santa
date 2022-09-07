4a72cc9879f7bcd3ed4f07b5dc4e1a4868c94f5a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-07 12:11:04 +0900

    ✨Feat : 프로필 수정 페이지 UI 제거

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,64 +1,36 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useNavigate } from "react-router-dom";
-import BaseButton from "src/components/BaseButton/BaseButton";
 import Dialog from "src/components/Dialog/Dialog";
 import styled from "styled-components";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 import { useUser } from "src/utils/localStorage";
-
-const MyReviews = [
-  "A산 리뷰",
-  "B산 리뷰",
-  "C산 리뷰 ",
-  "D산 리뷰",
-  "E산 리뷰",
-  "K산 리뷰",
-];
+import { BiEdit } from "react-icons/bi";
+import { LocalUser } from "src/types";
 
 const Profile = () => {
   const navigate = useNavigate();
-  const user = useUser();
-  console.log(user);
+  const user: LocalUser = useUser();
+
+  if (!user)
+    return (
+      <Dialog
+        confirm={() => navigate("/main/login")}
+        close={() => navigate(-1)}
+      />
+    );
   return (
     <Container>
-      {user ? null : (
-        <Dialog
-          confirm={() => navigate("/main/login")}
-          close={() => navigate("/main/")}
-        />
-      )}
-      <ProfileBox>
-        <img />
-        <BaseButton
-          text="프로필 수정"
-          width={35}
-          bgColor="lightgrey"
-          textColor="black"
-        />
-      </ProfileBox>
-      <MyReviewBox>
-        <h1>내 리뷰</h1>
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          {MyReviews.map((text, idx) => (
-            <SwiperSlide key={text}>
-              <DummyCard>{text}</DummyCard>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </MyReviewBox>
-      <InterestBox>
-        <h1>보고 싶은 산 목록</h1>
-        <div className="content">
-          <DummyCard2>관악산</DummyCard2>
-          <DummyCard2>설악산</DummyCard2>
-          <DummyCard2>한라산</DummyCard2>
+      <TitleBox>
+        <h1>프로필</h1>
+        <div onClick={() => navigate("/main/editprofile")}>
+          <BiEdit />
         </div>
-      </InterestBox>
+      </TitleBox>
+      <ProfileBox>
+        <img src={String(user.profileImageUrl)} alt="profile-img" />
+        <UserName>{user.username}</UserName>
+      </ProfileBox>
+      <MyReviewBox />
     </Container>
   );
 };
@@ -68,7 +40,6 @@ export default Profile;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   height: 100%;
   padding: 0rem 2rem;
   overflow: scroll;
@@ -79,46 +50,45 @@ const Container = styled.div`
   }
 `;
 
+const TitleBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: bold;
+
+  & > div {
+    padding: 0.5rem;
+    font-size: 2.5rem;
+    color: gray;
+  }
+`;
+
 const ProfileBox = styled.div`
   display: flex;
-  justify-content: center;
-  flex-direction: column;
   align-items: center;
+  flex-direction: column;
+  padding-bottom: 3rem;
+  border-bottom: solid 1px lightgray;
+
   width: 100%;
   margin-top: 2rem;
 
   img {
-    width: 15rem;
-    height: 15rem;
-    border-radius: 50%;
-    background-color: black;
+    width: 13rem;
+    height: 13rem;
+    border-radius: 100%;
     margin-bottom: 1rem;
+    object-fit: cover;
   }
 `;
 const MyReviewBox = styled.div`
   width: 100%;
   margin: 1rem 0rem;
-`;
-const InterestBox = styled.div`
-  width: 100%;
+  background-color: red;
 `;
 
-const DummyCard = styled.div`
-  width: 10rem;
-  height: 10rem;
-  background-color: #3a3f47;
-  color: white;
-  border-radius: 1rem;
-  padding: 1rem;
-`;
-
-const DummyCard2 = styled.div`
-  display: flex;
-  align-items: center;
-  /* justify-content: center; */
-  background-color: lightgray;
-  width: 100%;
-  height: 5rem;
-  border-radius: 0.5rem;
-  margin: 0.5rem 0rem;
+const UserName = styled.h2`
+  margin-top: 1rem;
+  font-size: 2rem;
+  font-weight: 500;
 `;
