7b641649a81e62591c6acc6822de287ce2882bf4
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-07 12:12:07 +0900

    ✨ Feat : 타입 추가

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,1 +1,12 @@
-export {};
+export interface User {
+  email: string;
+  username: string;
+  profileImageUrl: string;
+}
+
+export interface LocalUser {
+  email: string;
+  username: string;
+  profileImageUrl: string;
+  memberId: string;
+}
