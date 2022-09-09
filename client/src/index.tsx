2f72bf05a48e7335c177a08979d645374926e278
MoSangBin <mosangbin94@gmail.com>
2022-09-10 03:41:54 +0900

    :sparkles: Feat: dateFormat 유틸 함수 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import GlobalStyle from "./assets/css/GlobalStyle";
 import { store } from "./redux";
-import { worker } from "./mocks/browser";
 import { Modal } from "./components/Modal";
-
-// 실제 api 받을 때 주석 처리 해주세요.
-// if (process.env.NODE_ENV === "development") {
-//   worker.start();
-// }
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,7 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
