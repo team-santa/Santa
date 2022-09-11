b843b72c5a792b17f3f12acc819969377615496d
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-12 00:24:58 +0900

    ✨Feat : 타입 복구

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,12 +1,1 @@
-export interface User {
-  email: string;
-  username: string;
-  profileImageUrl: string;
-}
-
-export interface LocalUser {
-  email: string;
-  username: string;
-  profileImageUrl: string;
-  memberId: string;
-}
+export {};
